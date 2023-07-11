f822cb703a696a66ddba4cb2106bdf00345cdecd
Jimmy Jurado <jurado140@gmail.com>
2023-07-11 16:43:50 -0400

    Adds destroy spec test.

--- spec/requests/api/v1/items_spec.rb
+++ spec/requests/api/v1/items_spec.rb
@@ -52,7 +52,64 @@ RSpec.describe 'Items API' do
       expect(item_data[:data][:attributes]).to have_key(:name)
       expect(item_data[:data][:attributes][:name]).to be_an(String)
       expect(item_data[:data][:attributes][:name]).to eq(item_1.name)
+    end
 
+    it "can create an item" do
+      merchant_id = create(:merchant).id
+
+      item_params = ({
+        name: "iPhone",
+        description: "New Age Information Device",
+        unit_price: 99999999999.99,
+        merchant_id: merchant_id
+      })
+      header = {"CONTENT_TYPE" => "application/json"}
+
+      post "/api/v1/items", headers: header, params: JSON.generate(item: item_params)
+
+      created_item = Item.last
+      
+      expect(response).to be_successful
+      expect(created_item.name).to eq(item_params[:name])
+      expect(created_item.description).to eq(item_params[:description])
+      expect(created_item.unit_price).to eq(item_params[:unit_price])
+      expect(created_item.merchant_id).to eq(item_params[:merchant_id])
+    end
+
+    it 'can edit existing item' do
+      merchant_id = create(:merchant).id
+
+      id = create(:item, merchant_id: merchant_id).id
+      previous_name = Item.last.name
+      previous_description = Item.last.description
+      previous_price = Item.last.unit_price
+
+      item_params = ({
+        name: "iPhone",
+        description: "New Age Information Device",
+        unit_price: 99999999999.99,
+        merchant_id: merchant_id
+      })
+      header = {"CONTENT_TYPE" => "application/json"}
+
+      patch "/api/v1/items/#{id}", headers: header, params: JSON.generate(item: item_params)
+
+      item = Item.find_by(id: id)
+
+      expect(response).to be_successful
+      expect(item.name).to_not eq(previous_name)
+      expect(item.description).to_not eq(previous_description)
+      expect(item.unit_price).to_not eq(previous_price)
+    end
+
+    it "can delete an item" do
+      merchant_id = create(:merchant).id
+
+      id = create(:item, merchant_id: merchant_id).id
+
+      expect{ delete "/api/v1/items/#{id}" }.to change(Item, :count).by(-1)
+
+      expect{Item.find(id)}.to raise_error(ActiveRecord::RecordNotFound)
     end
   end
 end
