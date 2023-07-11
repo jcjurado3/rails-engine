ae282df58066df6b0f3925f0e7ad002a6c4e8780
Jimmy Jurado <jurado140@gmail.com>
2023-07-10 21:24:14 -0400

    cleans merchant items spec.

--- spec/requests/api/v1/merchant_items_spec.rb
+++ spec/requests/api/v1/merchant_items_spec.rb
@@ -17,5 +17,19 @@ RSpec.describe "Merchant Items API" do
     items = JSON.parse(response.body, symbolize_names: true)
 
     expect(items.count).to eq(5)
+
+    items.each do |item|
+      expect(item).to have_key(:id)
+      expect(item[:id]).to be_an(Integer)
+
+      expect(item).to have_key(:name)
+      expect(item[:name]).to be_an(String)
+
+      expect(item).to have_key(:description)
+      expect(item[:description]).to be_an(String)
+
+      expect(item).to have_key(:unit_price)
+      expect(item[:unit_price]).to be_an(Float)
+    end
   end
 end
