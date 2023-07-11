9cf7b07046c4dca80dbd1287a578c1d4537b3503
Jimmy Jurado <jurado140@gmail.com>
2023-07-11 16:44:18 -0400

    Updates controller with destroy action.

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -6,4 +6,23 @@ class Api::V1::ItemsController < ApplicationController
   def show
     render json: ItemSerializer.new(Item.find(params[:id]))
   end
+
+  def create
+    created_item = Item.create(item_params)
+    render json: ItemSerializer.new(created_item)
+  end
+
+  def update
+    updated_item = Item.update(params[:id], item_params)
+    render json: ItemSerializer.new(updated_item)
+  end
+
+  def destroy
+    render json: Item.delete(params[:id])
+  end
+
+  private
+    def item_params
+      params.require(:item).permit(:name, :description, :unit_price, :merchant_id)
+    end
 end
