da1becfdd71292c617934ad4a244c2da6f0838c3
Jimmy Jurado <jurado140@gmail.com>
2023-07-12 13:26:20 -0400

    Clean up

--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,12 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
+      get 'merchants/find', to: 'merchant_search#show'
+
       resources :merchants, only: [:index, :show] do
         resources :items, only: [:index], to: 'merchant_items#index'
       end
-
+  
       resources :items do
         resources :merchant, only: [:index], to: 'merchant_items#show'
       end
