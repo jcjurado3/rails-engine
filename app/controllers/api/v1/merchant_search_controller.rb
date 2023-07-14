bba732ce69c1beb69fa9e1fc8e2234b02957cf2d
Jimmy Jurado <jurado140@gmail.com>
2023-07-14 13:05:01 -0400

    last minute refactor.

--- app/controllers/api/v1/merchant_search_controller.rb
+++ app/controllers/api/v1/merchant_search_controller.rb
@@ -10,21 +10,5 @@ class Api::V1::MerchantSearchController < ApplicationController
     render json: MerchantSerializer.new(@merchant)
   end
 
-  private
 
-    def find_merchant
-      @merchant = Merchant.find_merchant_search(params[:name])
-        if @merchant == nil
-          error_object = Error.new("Invalid Search Keyword", "404")
-          render json: ErrorSerializer.serialize_error(error_object), status: error_object.status_code
-        end
-    end
-
-    def find_merchants
-      @merchants = Merchant.find_merchants_search(params[:name])
-        if @merchants == nil
-          error_object = Error.new("Invalid Search Keyword", "404")
-          render json: ErrorSerializer.serialize_error(error_object), status: error_object.status_code
-        end
-    end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,24 @@
 class ApplicationController < ActionController::API
   rescue_from ActiveRecord::RecordNotFound, with: :not_found_error_response
 
-
-
-  
-
   def not_found_error_response(error)
     error_object = Error.new(error.message, "404")
     render json: ErrorSerializer.serialize_error(error_object), status: error_object.status_code
+  end
+
+  def find_merchant
+    @merchant = Merchant.find_merchant_search(params[:name])
+      if @merchant == nil
+        error_object = Error.new("Invalid Search Keyword", "404")
+        render json: ErrorSerializer.serialize_error(error_object), status: error_object.status_code
+      end
+  end
+
+  def find_merchants
+    @merchants = Merchant.find_merchants_search(params[:name])
+      if @merchants == nil
+        error_object = Error.new("Invalid Search Keyword", "404")
+        render json: ErrorSerializer.serialize_error(error_object), status: error_object.status_code
+      end
   end
 end
